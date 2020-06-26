4b808f247ad18762a78590865a94259ea5a9a5af
IsaacRodgz <chack.rb@gmail.com>
2020-06-26 09:42:13 -0500

    FIX train

--- src/utils.py
+++ src/utils.py
@@ -24,3 +24,14 @@ def get_data(args, dataset, split='train'):
                            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
                        ]))
     return data
+
+
+def save_model(args, model, name=''):
+    name = name if len(name) > 0 else 'default_model'
+    torch.save(model, f'pre_trained_models/{name}.pt')
+
+
+def load_model(args, name=''):
+    name = name if len(name) > 0 else 'default_model'
+    model = torch.load(f'pre_trained_models/{name}.pt')
+    return model
